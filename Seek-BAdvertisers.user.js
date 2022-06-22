429366af62fe64cd14b42461106b2265b5a555e9
BoKu <BoKu@users.noreply.github.com>
2022-06-22 21:50:27 +0800

    Update Seek-BAdvertisers.user.js

--- Seek-BAdvertisers.user.js
+++ Seek-BAdvertisers.user.js
@@ -1,31 +1,46 @@
 // ==UserScript==
 // @name         Seek-BAdvertisers
 // @namespace    https://github.com/BoKu/Seek-BAdvertisers
-// @version      0.2
+// @version      0.3
 // @description  Hides agency adverts on Seek
 // @homepage     https://github.com/BoKu/Seek-BAdvertisers
 // @supportURL   https://github.com/BoKu/Seek-BAdvertisers/pulls
 // @author       BoKu
 // @match        *://*.seek.com.au/*
 // @match        *://*.seek.co.nz/*
-// @resource     json https://raw.githubusercontent.com/BoKu/Seek-BAdvertisers/main/badvertisers.json
-// @grant        GM_getResourceText
-// @run-at       document-idle
 // @license 	 Creative Commons Attribution-ShareAlike 3.0 Unported License.
 // ==/UserScript==
-function DeleteAds() {
-    const badvertisers = GM_getResourceText("json");
+(function() {
+    'use strict';
+    const jsonuri = 'https://raw.githubusercontent.com/BoKu/Seek-BAdvertisers/main/badvertisers.json';
     const strAdSection = "[data-search-sol-meta]";
     const strAdNameClass = ".l2mi890";
-    const JobsArray = document.querySelectorAll(strAdSection);
-    if(badvertisers){
-        JobsArray.forEach(Ad =>{
-            const badvertiseName = Ad.querySelector(strAdNameClass).innerText;
-            if(badvertisers.includes(badvertiseName)){
-                Ad.remove()
-                console.debug("Deleted Ad By:", badvertiseName)
-            }
-        })
-    }
-}
-setInterval(DeleteAds, 1000);
+
+    var DeleteAds = function (badvertisers) {
+        const JobsArray = document.querySelectorAll(strAdSection);
+        if(badvertisers){
+            JobsArray.forEach(Ad =>{
+                const badvertiseName = Ad.querySelector(strAdNameClass).innerText;
+                if(badvertisers.includes(badvertiseName)){
+                    Ad.remove();
+                    console.debug("Deleted Ad By:", badvertiseName);
+                }
+            })
+        }
+    };
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', jsonuri, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+        var status = xhr.status;
+        if (status === 200) {
+            console.debug(xhr.response);
+            DeleteAds(xhr.response);
+        } else {
+            console.debug(status, xhr.response);
+        }
+    };
+    xhr.send();
+
+})();
